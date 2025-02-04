cfe220f921055f3e65a26f563e750213af9999be
BPThiago <thiagoborges980@gmail.com>
2025-02-04 19:27:57 -0300

    Refactor: Indent on Views

--- src/views/EmpresasView.vue
+++ src/views/EmpresasView.vue
@@ -1,40 +1,40 @@
 <template>
-    <generic-table-view
-      :headers="headers"
-      :fetchData="fetchEmpresas"
-      @delete="handleDelete"
-    >
-      <template #actions="{ item }">
-        <button @click="deleteEmpresa(item.id)">Excluir</button>
-      </template>
-    </generic-table-view>
-  </template>
+  <generic-table-view
+    :headers="headers"
+    :fetchData="fetchEmpresas"
+    @delete="handleDelete"
+  >
+    <template #actions="{ item }">
+      <button @click="deleteEmpresa(item.id)">Excluir</button>
+    </template>
+  </generic-table-view>
+</template>
   
-  <script>
-  import GenericTableView from '@/components/GenericTableView.vue'
-  import EmpresaController from '@/controllers/EmpresaController.js'
-  
-  export default {
-    components: {
-      GenericTableView
+<script>
+import GenericTableView from '@/components/GenericTableView.vue'
+import EmpresaController from '@/controllers/EmpresaController.js'
+
+export default {
+  components: {
+    GenericTableView
+  },
+  data() {
+    return {
+      headers: [
+        { key: 'Id', label: 'ID' },
+        { key: 'Nome', label: 'Nome' },
+      ]
+    }
+  },
+  methods: {
+    fetchEmpresas() {
+      const empresasController = new EmpresaController();
+      return empresasController.ObterTodos();
     },
-    data() {
-      return {
-        headers: [
-          { key: 'Id', label: 'ID' },
-          { key: 'Nome', label: 'Nome' },
-        ]
-      }
-    },
-    methods: {
-      fetchEmpresas() {
-        const empresasController = new EmpresaController();
-        return empresasController.ObterTodos();
-      },
-      handleDelete(id) {
-        // TODO: Vamo add algo aqui ainda carma
-        console.log('Deletar empresa com id', id)
-      }
+    handleDelete(id) {
+      // TODO: Vamo add algo aqui ainda carma
+      console.log('Deletar empresa com id', id)
     }
   }
-  </script>
+}
+</script>
--- src/views/OrientadoresView.vue
+++ src/views/OrientadoresView.vue
@@ -1,42 +1,42 @@
 <template>
-    <generic-table-view
-      :headers="headers"
-      :fetchData="fetchOrientadores"
-      @delete="handleDelete"
-    >
-      <template #actions="{ item }">
-        <button @click="deleteOrientador(item.id)">Excluir</button>
-      </template>
-    </generic-table-view>
-  </template>
+  <generic-table-view
+    :headers="headers"
+    :fetchData="fetchOrientadores"
+    @delete="handleDelete"
+  >
+    <template #actions="{ item }">
+      <button @click="deleteOrientador(item.id)">Excluir</button>
+    </template>
+  </generic-table-view>
+</template>
   
 <script>
 import GenericTableView from '@/components/GenericTableView.vue'
 import OrientadorController from '@/controllers/OrientadorController.js'
   
 export default {
-    components: {
-      GenericTableView
-    },
-    data() {
-      return {
-        headers: [
-          { key: 'Id', label: 'ID' },
-          { key: 'Nome', label: 'Nome' },
-          { key: 'Email', label: 'Email' },
-          { key: 'Telefone', label: 'Telefone' },
-        ]
-      }
-    },
-    methods: {
-      fetchOrientadores() {
-        const orientadoresController = new OrientadorController();
-        return orientadoresController.ObterTodos();
-      },
-      handleDelete(id) {
-        // TODO: Vamo add algo aqui ainda carma
-        console.log('Deletar orientador com id', id)
-      }
+  components: {
+    GenericTableView
+  },
+  data() {
+    return {
+      headers: [
+        { key: 'Id', label: 'ID' },
+        { key: 'Nome', label: 'Nome' },
+        { key: 'Email', label: 'Email' },
+        { key: 'Telefone', label: 'Telefone' },
+      ]
     }
+  },
+  methods: {
+    fetchOrientadores() {
+      const orientadoresController = new OrientadorController();
+      return orientadoresController.ObterTodos();
+    },
+    handleDelete(id) {
+      // TODO: Vamo add algo aqui ainda carma
+      console.log('Deletar orientador com id', id)
+    }
+  }
 }
 </script>
